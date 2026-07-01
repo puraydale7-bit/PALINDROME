86a573e8c28a6f9e8f18fc158198de641662e9c2
puraydale7-bit <puraydale7@gmail.com>
2026-07-01 12:54:40 +0800

    Add files via upload

---
+++ FINAL ACT1.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], size = 0;
+    char ch;
+
+    printf("Enter numbers separated by spaces: ");
+    
+    while (scanf("%d%c", &arr[size], &ch) == 2) {
+        size++;
+        if (ch == '\n') break; 
+    }
+
+    int left = 0, right = size - 1;
+    while (left < right) {
+        if (arr[left] != arr[right]) {
+            printf("NOT PALINDROME\n");
+            return 0;
+        }
+        left++;
+        right--;
+    }
+
+    printf("PALINDROME\n");
+    return 0;
+}
